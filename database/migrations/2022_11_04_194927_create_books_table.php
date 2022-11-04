595fe661b49f84c652be4b23b1d05d6b65f6a1ba
Uthman Bakar <uthmanb@outlook.com>
2022-11-04 21:48:48 +0100

    Created data objects Book, CheckOut, Role, UserRole, BookCheckStatus along with corresponding contollers and migrations. Ran migrations to create database and tables

---
+++ database/migrations/2022_11_04_194927_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('isbn');
+            $table->string('cover_image');
+            $table->integer('revision_number');
+            $table->timestamp('published_date');
+            $table->string('publisher');
+            $table->string('author');
+            $table->string('genre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ database/migrations/2022_11_04_194954_create_check_outs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('check_outs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('book_id');
+            $table->timestamp('check_out_date');
+            $table->timestamp('expected_date')->nullable();
+            $table->tinyInteger('check_out_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('check_outs');
+    }
+};
